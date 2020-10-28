67eb212f65d91428e826fe89b5d35612e94386ec
Ethan Gallant <ethan@exclnetworks.com>
2020-10-28 17:07:30 -0400

    Remove array returns

--- handlers/TimerHandler.js
+++ handlers/TimerHandler.js
@@ -4,23 +4,22 @@ const {
 
 const TimerDSConnector = require("../connectors/datastore/TimerDSConnector");
 const RedisManager = require('../connectors/RedisConnector');
-
 const NotificationConnector = require('../connectors/NotificationConnector');
 const logger = require('../Logger');
 
 exports.handleReset = async (socket,id) => {
     if (typeof id !== "string")
         return;
-
+    if(id.length < 1)
+        return;
     let timer = await TimerDSConnector.get(id, socket.office);
-    if (!timer || !timer[0])
+    if(!timer)
         return;
     logger.debug(`[TimerReset] Resetting Timer ${id}`);
 
     if (typeof timer.notification === "string" && timer.notification) {
         await NotificationConnector.cancelNotification(timer.notification);
     }
-
     timer.expires_at = await (new Date().getTime() + (timer.length * 1000));
     timer.notification = await NotificationConnector.scheduleNotification(timer);
     logger.debug(`[TimerReset] Resetting Timer ${id} to it's set time of ${timer.length}. Timer data is ${JSON.stringify(timer)}`);
@@ -29,13 +28,15 @@ exports.handleReset = async (socket,id) => {
 };
 
 exports.handleZero = async (socket, id) => {
-    console.debug(`[TimerZero] Zeroing Timer ${id}`);
+    if(id.length < 1)
+        return;
     let timer = await TimerDSConnector.get(id, socket.office);
-    if (!timer || !timer[0])
+    if(!timer)
         return;
     logger.debug(`[TimerZero] Zeroing Timer ${id}`);
     timer.expires_at = 0;
     await TimerDSConnector.update(timer);
+
     await RedisManager.emitEvent(socket.office, 'timer.updated', timer);
     if (typeof timer.notification === "string" && timer.notification) {
         await NotificationConnector.cancelNotification(timer.notification);
@@ -43,8 +44,10 @@ exports.handleZero = async (socket, id) => {
 };
 
 exports.handleDelete = async (socket, id) => {
+    if(id.length < 1)
+        return;
     let timer = await TimerDSConnector.get(id, socket.office);
-    if (!timer || !timer[0])
+    if(!timer)
         return;
     logger.debug(`[TimerDelete] Deleting timer with id ${id}`);
     await TimerDSConnector.delete(timer, socket.office);
@@ -55,11 +58,22 @@ exports.handleDelete = async (socket, id) => {
 };
 
 exports.handleCreate = async (socket, data) => {
+    if (
+        typeof data.name !== "string" ||
+        typeof data.length !== "string" ||
+        data.name.length < 1 ||
+        data.name.length > 50 ||
+        isNaN(data.length) ||
+        parseInt(data.length) < 30 ||
+        parseInt(data.length) > 172800
+    ) {
+        return;
+    }
+
     let timer = {
         id: await uuidv4(),
         name: data.name,
         length: parseInt(data.length),
-        color: data.color,
         office: socket.office,
         expires_at: 0
     };
